9199a635397793258892494a1ccb6a36af9fa2dd
Daniel Amaya <damayalop@gmail.com>
2021-11-08 15:33:42 -0600

    Error arreglado: No se mostraba los estilos predeterminados

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PersonaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+/* MODULO PERSONAS */
+Route::get('/personas/{vista}', [PersonaController::class, 'ShowPersona']);
